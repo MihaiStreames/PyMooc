6b6e387223e4d64cc0ac98b9a5e3b4db31bc816e
Mihai <moreliciouscod@gmail.com>
2022-10-07 14:44:18 +0200

    4.5 done

---
+++ 4/4.5.5 upylab 4.4.py
@@ -0,0 +1,10 @@
+import random
+
+def alea_dice(s):
+    random.seed(s)
+
+    dices = [random.randint(1,6), random.randint(1,6), random.randint(1,6)]
+    if sorted(dices) == [1, 2, 4]:
+        return True
+    else:
+        return False
