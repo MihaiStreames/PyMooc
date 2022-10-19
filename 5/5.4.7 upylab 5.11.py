0c27ee094dc2decb15754d2cddb6e25280d37a9d
Mihai <moreliciouscod@gmail.com>
2022-10-19 19:47:39 +0200

    taboundymek

---
+++ 5/5.4.7 upylab 5.11.py
@@ -0,0 +1,13 @@
+def intersection(v, w):
+    ix = ''
+    min_ = min(len(v), len(w))
+    for s in range(len(v)):
+        if len(ix) >= min_ - s:
+            break
+        for e in range(s + len(ix), min_):
+            t = v[s:e+1]
+            if t not in w:
+                break
+            if len(t) >= len(ix):
+                ix = t
+    return ix
